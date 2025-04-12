007a06f330f4156cb19dedae7f102dabcde42519
Garry Ledwith <S00256356@atu.ie>
2025-04-12 13:54:33 +0000

    Created Xaml interface

--- RestaurantBooking/MainWindow.xaml.cs
+++ RestaurantBooking/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace RestaurantBooking
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
