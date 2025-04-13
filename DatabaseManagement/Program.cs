b880e617e9bf1147d48477e40edb5db5f047d2a6
Garry Ledwith <S00256356@atu.ie>
2025-04-13 08:04:04 +0000

    Adding database management folder

--- DatabaseManagement/Program.cs
+++ DatabaseManagement/Program.cs
@@ -15,24 +15,46 @@ namespace DatabaseManagement
             // new instance of RestaurantData class for db connection 
             RestaurantData db = new RestaurantData();
 
+            // try, catch for when connecting to the database 
             try
             {
                 using (db)
                 {
+                    // creating Customer objects 
                     Customer c1 = new Customer() { Name = "Garry Ledwith", ContactNumber = "086-123-4556" };
                     Customer c2 = new Customer() { Name = "Jimmy Doyle", ContactNumber = "087-323-4321" };
                     Customer c3 = new Customer() { Name = "Ellie Smith", ContactNumber = "086-863-9832" };
 
                     Console.WriteLine("Created the customers");
 
+                    // adding customer objects to the Customers database 
                     db.Customers.Add(c1);
                     db.Customers.Add(c2);
                     db.Customers.Add(c3);
+
+                    // creating Booking objects 
+                    Booking b1 = new Booking() { BookingDate = DateTime.Now, NoOfParticipants = 5, CustomerId = 1};
+                    Booking b2 = new Booking() { BookingDate = DateTime.Now, NoOfParticipants = 3, CustomerId = 2};
+
+                    // adding Bookings to the Bookings database 
+                    db.Bookings.Add(b1);
+                    db.Bookings.Add(b2);
+
+                    // confirmation messages 
+                    Console.WriteLine("Added Bookings");
+
+                    // save changes made to database 
+                    db.SaveChanges();
+
+                    // confirmation messages 
+                    Console.WriteLine("Changes saved");
+                    Console.WriteLine("Press enter to continue");
+                    Console.WriteLine("");
                 }
             }
             catch (Exception ex) 
             {
-            
+                Console.WriteLine("Sorry an erroroccured when connecting to the database: " + ex.Message);
             }
         }
     }
--- RestaurantBooking/RestaurantData.cs
+++ RestaurantBooking/RestaurantData.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace RestaurantBooking
 {
-    internal class RestaurantData
+    /// <summary>
+    /// Restaurant Data Class which uses DbContext to connect to a database 
+    /// 
+    /// </summary>
+    public class RestaurantData : DbContext 
     {
+        // constructor 
+        public RestaurantData() : base("RestaurantData_OOD2024Exam_GL") { } // Name of the database 
+        public DbSet<Booking> Bookings { get; set;} // creates a Bookings table in the database 
+        public DbSet<Customer> Customers { get; set;}  // creates a Customer table in the database 
     }
 }
